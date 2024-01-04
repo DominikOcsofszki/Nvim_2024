08c5fd380635a822af609000b70d6e02d6a00499
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-04 12:51:07 +0100

    Adding autocomplete

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -10,3 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader= " "
+require("lazy").setup("plugins")
---
+++ lua/plugins/folke.lua
@@ -0,0 +1,9 @@
+return {
+
+  "folke/which-key.nvim",
+{ "folke/neodev.nvim", opts = {} }
+
+
+
+
+}
