fbe36a0a1a1dbadbfef6889d478456808c96fb91
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-07 11:58:31 +0100

    working lsp with mason

--- lua/configs/functions.lua
+++ lua/configs/functions.lua
@@ -1,2 +1,3 @@
 
 -- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = require("nvim-tree.api").tree.open })
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, { callback =  })
---
+++ lua/plugins/mason-lsp.lua
@@ -0,0 +1,40 @@
+
+return {
+    {
+        "neovim/nvim-lspconfig",
+        dependencies =
+        {
+            "williamboman/mason-lspconfig.nvim",
+            dependencies =
+            { "williamboman/mason.nvim" }
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls" },
+            })
+            require("mason-lspconfig").setup_handlers {
+                -- The first entry (without a key) will be the default handler
+                -- and will be called for each installed server that doesn't have
+                -- a dedicated handler.
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {}
+                end,
+                -- Next, you can provide a dedicated handler for specific servers.
+                -- For example, a handler override for the `rust_analyzer`:
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim" }
+                                }
+                            }
+                        }
+                    }
+                end,
+            }
+        end
+    }
+}
