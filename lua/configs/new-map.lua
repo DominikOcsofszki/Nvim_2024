bc19e75c64d403d845d3b65aef4bdd3ecb013f64
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-03-03 21:49:12 +0100

    update

--- lua/configs/new-map.lua
+++ lua/configs/new-map.lua
@@ -14,3 +14,25 @@ vim.keymap.set('n', '<leader>j1',':%!jq keys<cr>',{})
 vim.keymap.set('n', '<leader>j2','yi"u :%!jq .<c-r>*<cr>',{})
 vim.keymap.set('n', '<leader>j3','yyu :%!jq .<c-r>*',{})
 
+-- set-option -g -q mouse on
+
+-- jq -r 'paths(scalars) | map(if type == "number" then "[\(. | tostring)]" else "." + tostring end) | join("")' json_example.json
+vim.keymap.set('n', '<leader>j3','yyu :%!jq .<c-r>*',{})
+
+
+
+-- ='jq -r \'paths(scalars) | map(if type == "number" then "[\(. | tostring)]" else "." + tostring end) | join("")\' json_example.json'
+-- alias jgf='jq -r \'paths(scalars) | map(if type == "number" then "[\(. | tostring)]" else "." + tostring end) | join("")\' json_example.json'
+
+
+-- cat json_example.json |jq $(jq -r 'paths(scalars) | map(if type == "number" then "[\(. | tostring)]" else "." + tostring end) | join("")' json_example.json | fzf)
+
+-- cat json_example.json |jq $(jq -r 'paths(scalars) | map(if type == "number" then "[\(. | tostring)]" else "." + tostring end) | join("")' json_example.json | fzf)
+
+-- alias show_keys='jq -r '\''paths(scalars) | map(if type == "number" then "[\(. | tostring)]" else "." + tostring end) | join("")'\'''
+-- alias show_jq='jq $(show_keys)'
+-- vim.keymap.set('n', ',j', function()
+--     vim.cmd('jq -r '\''paths(scalars) | map(if type == "number" then "[\(. | tostring)]" else "." + tostring end) | join("")'\''')
+--     end,
+--     {})
+
